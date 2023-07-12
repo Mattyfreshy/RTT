cc4e8be2eccf914e8142836b04444720be24a469
Matthew Zou <mzou1212@gmail.com>
2023-07-12 19:02:41 -0400

    loopback test. MacOS not supported

--- RTT.py
+++ RTT.py
@@ -1,10 +1,13 @@
 from dotenv import load_dotenv
 import time
 import os
+from sys import platform
 
 import openai
 import speech_recognition as sr
 import whisper
+import pyaudio
+import wave
 
 # Load .env file
 load_dotenv()
@@ -12,7 +15,7 @@ load_dotenv()
 # OpenAI API Key
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def transcribe_audio_to_text(filename):
+def transcribe_google(filename):
     '''Transcribes audio to text using Google's Speech Recognition API'''
     recognizer = sr.Recognizer()
     with sr.AudioFile(filename) as source:
@@ -27,12 +30,32 @@ def transcribe_audio_to_text(filename):
 def transcribe_whisper(model: whisper, filename):
     '''Transcribes audio to text using Whisper'''
     try:
-        result = model.transcribe(filename, fp16=False, language='english', condition_on_previous_text=False)
+        if platform == 'win32':
+            # Windows
+            fp16=True
+        else:
+            # Mac OS/Linux
+            fp16=False
+        
+        result = model.transcribe(filename, fp16=fp16, language='english', condition_on_previous_text=False)
         return result["text"]
     except Exception as e:
         print("[Whisper] An error occurred: {}".format(e))
         # print("...")
             
+
+def transcribe_audio_to_text(model, filename):
+    # Transcribe audio to text. As of now, Google's Speech Recognition API is faster than Whisper
+    if model is None:
+        text = transcribe_google(filename)
+    else:
+        text = transcribe_whisper(model, filename)
+
+    # Print transcription
+    if text:
+        print(f"Transcription: {text}")
+        # print(text)
+
 def generate_response(prompt):
     '''Generates a response to a prompt using OpenAI's Davinci API'''
     response = openai.Completion.create(
@@ -45,12 +68,11 @@ def generate_response(prompt):
     )
     return response['choices'][0]['text']
 
-def RTT(model, microphone):
-    '''Records and transcribes audio to text'''
+def RTT_mic(model: whisper, microphone: sr.Microphone):
+    '''Records and transcribes mic audio to text'''
     try:
         # Record audio
         filename = 'RTT.wav'
-        # print("Recording...")
         with microphone as source:
             recognizer = sr.Recognizer()
             recognizer.adjust_for_ambient_noise(source)
@@ -61,14 +83,57 @@ def RTT(model, microphone):
                 f.write(audio.get_wav_data())
             
         # Transcribe audio to text. As of now, Google's Speech Recognition API is faster than Whisper
-        # text = transcribe_audio_to_text(filename)
-        text = transcribe_whisper(model, filename)
-        if text:
-            print(f"Transcription: {text}")
-            # print(text)
+        transcribe_audio_to_text(model, filename)
                 
     except Exception as e:
-        print("[Voice Assistant] An error occurred: {}".format(e))
+        print("[RTT_mic] An error occurred: {}".format(e))
+
+def RTT_system(model: whisper):
+    '''Records and transcribes system audio to text'''
+    try:
+        # Record audio
+        CHUNK = 1024
+        FORMAT = pyaudio.paInt16
+        CHANNELS = 2
+        RATE = 44100
+        RECORD_SECONDS = 5
+        WAVE_OUTPUT_FILENAME = "output.wav"
+
+        p = pyaudio.PyAudio()
+
+        stream = p.open(format=FORMAT,
+                        channels=CHANNELS,
+                        rate=RATE,
+                        input=True,
+                        frames_per_buffer=CHUNK)
+
+        print("* recording")
+
+        frames = []
+
+        for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+            data = stream.read(CHUNK)
+            frames.append(data)
+
+        print("* done recording")
+
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+        with wave.open(WAVE_OUTPUT_FILENAME, 'wb') as wf:
+            wf.setnchannels(CHANNELS)
+            wf.setsampwidth(p.get_sample_size(FORMAT))
+            wf.setframerate(RATE)
+            wf.writeframes(b''.join(frames))
+
+
+        # Transcribe audio to text. As of now, Google's Speech Recognition API is faster than Whisper
+        transcribe_audio_to_text(model, WAVE_OUTPUT_FILENAME)
+
+        pass
+    except Exception as e:
+        print("[RTT_system] An error occurred: {}".format(e))
     
 def translate():
     '''Translates audio to English using OpenAI's Whisper API'''
@@ -99,13 +164,16 @@ def main(loop=False):
     print("\033[32mLoading Whisper Model...\033[37m")
     model = whisper.load_model('base')         # Whisper model size (tiny, base, small, medium, large)
     print("\033[32mRecording...\033[37m(Ctrl+C to Quit)\033[0m")
+    
     # Debugging: Print all microphone names
     # print(sr.Microphone.list_microphone_names())
     microphone = sr.Microphone(device_index=1, sample_rate=16000)  # Microphone device index
+    
     while True:    
         # Live Transcription w/ Whisper
         try:
-            RTT(model, microphone)
+            # Set model = None if you want to use Google's Speech Recognition API
+            RTT_mic(model, microphone)
         except (KeyboardInterrupt, SystemExit): break
 
 if __name__ == "__main__":
---
+++ system_record.py
@@ -0,0 +1,20 @@
+import soundcard as sc
+import soundfile as sf
+
+OUTPUT_FILE_NAME = "output.wav"    # file name.
+SAMPLE_RATE = 48000              # [Hz]. sampling rate.
+RECORD_SEC = 5                  # [sec]. duration recording audio.
+
+
+# Currently only works with Windows/linux due to MacOS not supporting loopback recording functionality.
+print(sc.all_speakers())
+print(sc.all_microphones())
+print(sc.default_speaker())
+
+# record audio with loopback from default speaker.
+with sc.get_microphone(id=str(sc.default_speaker().name), include_loopback=True).recorder(samplerate=SAMPLE_RATE) as mic:
+    # record audio with loopback from default speaker.
+    data = mic.record(numframes=SAMPLE_RATE*RECORD_SEC)
+    
+    # change "data=data[:, 0]" to "data=data", if you would like to write audio as multiple-channels.
+    sf.write(file=OUTPUT_FILE_NAME, data=data[:, 0], samplerate=SAMPLE_RATE)
