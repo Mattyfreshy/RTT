da5408ff621db04c6ccc3857ce7a7cb98fab65ed
Matthew Zou <mzou1212@gmail.com>
2023-07-13 17:02:04 -0400

    update tests

--- system_record.py
+++ system_record.py
@@ -4,6 +4,7 @@ import soundfile as sf
 OUTPUT_FILE_NAME = "output.wav"    # file name.
 SAMPLE_RATE = 48000              # [Hz]. sampling rate.
 RECORD_SEC = 5                  # [sec]. duration recording audio.
+GAIN_FACTOR = 2.0               # gain factor for amplifying audio. (1.0 = no amplification)
 
 
 # Currently only works with Windows/linux due to MacOS not supporting loopback recording functionality.
@@ -15,6 +16,9 @@ print('Default Speaker: \n', sc.default_speaker())
 with sc.get_microphone(id=str(sc.default_speaker().name), include_loopback=True).recorder(samplerate=SAMPLE_RATE) as mic:
     # record audio with loopback from default speaker.
     data = mic.record(numframes=SAMPLE_RATE*RECORD_SEC)
+
+    # Amplify the audio
+    data = data * GAIN_FACTOR
     
     # change "data=data[:, 0]" to "data=data", if you would like to write audio as multiple-channels.
     sf.write(file=OUTPUT_FILE_NAME, data=data[:, 0], samplerate=SAMPLE_RATE)
